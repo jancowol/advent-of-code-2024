116db6b3a711b223ac427ea9d75e54685f6b871b
Janco Wolmarans <janco.wolmarans@valr.com>
2024-12-01 18:36:22 +0200

    some refactoring

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::fs;
 
 pub fn part1() -> u32 {
@@ -5,6 +6,15 @@ pub fn part1() -> u32 {
     let (mut list1, mut list2) = split_lists(&data);
     let sum = calc_diffs(&mut list1, &mut list2);
     sum
+}
+
+pub fn part2() -> i32 {
+    let data = fs::read_to_string("data/day-1-1.txt").unwrap();
+    let (mut list1, mut list2) = split_lists(&data);
+    let l1 = to_i32_vec(&mut list1);
+    let l2 = to_i32_vec(&mut list2);
+    let similarity_score = calc_similarity_score(l1, l2);
+    similarity_score
 }
 
 fn split_lists(data: &String) -> (Vec<&str>, Vec<&str>) {
@@ -31,12 +41,24 @@ fn calc_diffs(list1: &mut Vec<&str>, list2: &mut Vec<&str>) -> u32 {
     diffs.sum()
 }
 
+fn to_i32_vec(vec: &mut Vec<&str>) -> Vec<i32> {
+    vec.iter().map(|s| s.parse::<i32>().unwrap()).collect_vec()
+}
+
+fn calc_similarity_score(list1: Vec<i32>, list2: Vec<i32>) -> i32 {
+    list1
+        .iter()
+        .map(|v1| {
+            let count = list2.iter().filter(|v2| v1.eq(v2)).count() as i32;
+            *v1 * count
+        })
+        .sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::day1::calc_diffs;
-    use itertools::Itertools;
-    use std::ops::Mul;
 
     #[test]
     fn sums_all_diffs() {
@@ -51,24 +73,9 @@ mod tests {
         let mut list1 = vec!["3", "4", "2", "1", "3", "3"];
         let mut list2 = vec!["4", "3", "5", "3", "9", "3"];
 
-        let l1 = list1
-            .iter()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect_vec();
-
-        let l2 = list2
-            .iter()
-            .map(|s| s.parse::<i32>().unwrap())
-            .collect_vec();
-
-        let foo: i32 = l1
-            .iter()
-            .map(|value| {
-                let count = l2.iter().filter(|x| value.eq(x)).count() as i32;
-                *value * count
-            })
-            .sum();
-
-        assert_eq!(foo, 31);
+        let l1 = to_i32_vec(&mut list1);
+        let l2 = to_i32_vec(&mut list2);
+        let similarity_score = calc_similarity_score(l1, l2);
+        assert_eq!(similarity_score, 31);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod day1;
 
 fn main() {
     println!("day 1, problem 1: {}", day1::part1());
+    println!("day 1, problem 2: {}", day1::part2());
 }
