7245aa45dc909937df17ccf392b3a1c4f330df9f
Janco Wolmarans <janco.wolmarans@valr.com>
2024-12-07 10:43:10 +0200

    refactor for part2

--- src/day2.rs
+++ src/day2.rs
@@ -2,25 +2,47 @@ use itertools::{FoldWhile, Itertools};
 use std::fs;
 
 pub(crate) fn part1() -> usize {
-    let data = fs::read_to_string("data/day-2-1.txt").unwrap();
-    count_safe_reports(data.as_str())
+    let reports = read_reports();
+    let safe_count = reports.into_iter().filter(|r| is_safe(r.to_vec())).count();
+    safe_count
 }
 
-fn count_safe_reports(input: &str) -> usize {
-    let reports = input
-        .split('\n')
+// pub(crate) fn part2() -> usize {
+//     count_safe_reports()
+// }
+
+fn read_reports() -> Vec<Vec<i32>> {
+    let data = fs::read_to_string("data/day-2-1.txt").unwrap();
+    data.split('\n')
         .map(|l| {
             l.split_whitespace()
                 .map(|n| n.parse::<i32>().unwrap())
                 .collect_vec()
         })
-        .collect_vec();
-    let safe_count = reports.into_iter().filter(|r| is_safe(r.to_vec())).count();
-    safe_count
+        .collect_vec()
 }
 
 fn is_safe(report: Vec<i32>) -> bool {
-    let mut diffs = report.windows(2).map(|w| w[0] - w[1]);
+    let diffs = report.windows(2).map(|w| w[0] - w[1]);
+    diff_seq_is_safe(diffs)
+}
+
+fn safe_with_dampener(report: Vec<i32>) -> bool {
+    let mut dampened_reports = report
+        .iter()
+        .enumerate()
+        .map(|(i, val)| report.iter().take(i + 1).chain(report.iter().skip(i + 2)));
+    for dampened_report in dampened_reports {
+        let report_values = dampened_report.collect_vec();
+        let diffs = report_values.windows(2).map(|w| w[0] - w[1]);
+        if diff_seq_is_safe(diffs) {
+            return true;
+        }
+    }
+    false
+}
+
+fn diff_seq_is_safe(mut diffs: impl Iterator<Item = i32>) -> bool {
     let all_safe = diffs.fold_while(None, |prev: Option<i32>, next: i32| {
         let diff = next.abs();
         let prev_signum = match prev {
@@ -53,7 +75,8 @@ mod tests {
     8 6 4 4 1
     1 3 6 7 9"#;
 
-        let safe_count = count_safe_reports(input);
+        let reports = read_reports();
+        let safe_count = reports.into_iter().filter(|r| is_safe(r.to_vec())).count();
         assert_eq!(safe_count, 2);
     }
 
@@ -97,5 +120,17 @@ mod tests {
 
         let unsafe11 = vec![1, 4, 6, 8, 8];
         assert!(!is_safe(unsafe11));
+    }
+
+    #[test]
+    fn is_safe_with_dampener() {
+        let safe1 = vec![1, 3, 2, 4, 5];
+        assert!(safe_with_dampener(safe1));
+
+        let safe2 = vec![8, 6, 4, 4, 1];
+        assert!(safe_with_dampener(safe2));
+
+        let unsafe1 = vec![1, 2, 7, 8, 9];
+        assert!(!safe_with_dampener(unsafe1));
     }
 }
